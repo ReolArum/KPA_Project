95f1807399a0d8e4fdeae76c1871d1d7b483d870
ReolArum <genspark_dev@genspark.ai>
2026-03-15 12:22:04 +0000

    feat: 더미 스킬 쿨타임 추가 (방어자세 3턴, 빠른발놀림 2턴)

--- My project/Assets/_Project/Scripts/BattleSceneController.cs
+++ My project/Assets/_Project/Scripts/BattleSceneController.cs
@@ -751,12 +751,12 @@ public class BattleSceneController : MonoBehaviour
 
         var s2 = ScriptableObject.CreateInstance<SkillData>();
         s2.skillName = "방어자세"; s2.category = SkillCategory.Defense;
-        s2.weight = 30; s2.damageMultiplier = 0.5f;
+        s2.weight = 30; s2.damageMultiplier = 0.5f; s2.cooldownTurns = 3;
         s2.animationTrigger = "Attack_Guard"; s2.hitAnimationTrigger = "Hit";
 
         var s3 = ScriptableObject.CreateInstance<SkillData>();
         s3.skillName = "빠른발놀림"; s3.category = SkillCategory.Mobility;
-        s3.weight = 40; s3.damageMultiplier = 0.8f; s3.avAdvance = 500f;
+        s3.weight = 40; s3.damageMultiplier = 0.8f; s3.avAdvance = 500f; s3.cooldownTurns = 2;
         s3.animationTrigger = "Attack_Step"; s3.hitAnimationTrigger = "Hit";
 
         unit.equippedSkills.Add(s1);
